c23c30027c03dea95cc977f58164fa0631d13330
766809dev <44704593+766809dev@users.noreply.github.com>
2018-12-13 13:44:12 +0000

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,7 @@
 @echo off 
 
 echo —Delete ROOT.war 
-del %DEPLOYMENT_TARGET%\webapps\ROOT.war
+del d:\home\site\wwwroot\webapps\ROOT.war
 echo ---Copy File to webapp folder
 copy d:\home\site\repository\sample1\target\*.war %DEPLOYMENT_TARGET%\webapps\ROOT.war
 ;echo ---Rename File to ROOT.war 
